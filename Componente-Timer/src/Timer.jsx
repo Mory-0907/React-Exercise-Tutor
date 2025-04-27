643177552405981bea2eece0cff5855db36139ad
Morena D'Asta <morena.work.0907@gmail.com>
2025-04-27 17:40:03 +0200

    Componente-Timer

---
+++ Componente-Timer/src/Timer.jsx
@@ -0,0 +1,34 @@
+import { useEffect } from "react";
+import { useState } from "react";
+import { useRef } from "react";
+
+export function Timer() {
+  const [active, setActive] = useState(true);
+  const [seconds, setSeconds] = useState(0);
+  const intervalRef = useRef(null);
+
+  const stopTimer = () => {
+    setActive(false);
+  };
+
+  const resumeTimer = () => {
+    setActive(true);
+  };
+
+  useEffect(() => {
+    if (active) {
+      intervalRef.current = setInterval(() => {
+        setSeconds((prev) => prev + 1);
+      }, 1000);
+    }
+    return () => clearInterval(intervalRef.current);
+  }, [active]);
+
+  return (
+    <div>
+      <h2>Timer: {seconds} seconds</h2>
+      <button onClick={stopTimer}>Stop</button>
+      <button onClick={resumeTimer}>Resume</button>
+    </div>
+  );
+}
